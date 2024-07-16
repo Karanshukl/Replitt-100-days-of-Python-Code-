582dea6d7da376acbe305c9633a0f72b1746fff1
Karan7680 <113102025+Karanshukl@users.noreply.github.com>
2024-07-16 14:04:53 +0530

    Create Day_8.py

---
+++ Day_8.py
@@ -0,0 +1,23 @@
+# Affirmations (or insults) Generator
+print("Insult Generator")
+
+name = input("What is Your Name: ")
+if (name == "Himanshu" or name == "himanshu"):
+  print("Whats Up Himanshu")
+  howAreyou = input("How are You? ")
+  if (howAreyou == "good" or howAreyou == "Good" or howAreyou == "fine"):
+    print("You're creepy")
+  else:
+    print("It's Ok ")
+
+elif ((name == "keshav") or (name == "Keshav")):
+  print("how are you? ")
+  HowAreYou = input("How are You? ")
+  if (HowAreYou == "good" or HowAreYou == "Good" or HowAreYou == "fine"):
+    print("But you're very Bald")
+  else:
+    print("Good, It's Ok")
+
+else:
+ print("You're not that type of person")
+
