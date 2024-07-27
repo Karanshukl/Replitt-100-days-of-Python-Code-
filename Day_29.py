2fe613d2de19a9cd4f9c3174b45e276b3e75be92
Karan7680 <113102025+Karanshukl@users.noreply.github.com>
2024-07-27 21:57:14 +0530

    Create Day_29.py

---
+++ Day_29.py
@@ -0,0 +1,4 @@
+def printcolor():
+  print("\033[33m""Super Subroutine\n\n""with my", "\033[95m""new program","\033[0m""\033[33m""I can just\ncall red "'("and")'"\033[31m and \033[0m""\033[33m""that word will\n""appear in the color i set it to.\n\n""With no \033[34mweird gaps\033[0m.\n\n""\033[33m""Epic!")
+
+printcolor()
